39ca4ad097bf5851b34a2bfd7a792fe71f9aab74
Mint Missy <dawidkostka480@gmail.com>
2024-05-31 23:01:15 +0200

    feat: reproduce ERR_INVALID_URL bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'err-invalid-url-repro';
 }
